a4af61fec8e24c6275b2a8e5618cf2e1c1c9be01
Mike Barberry <mikebarberry@protonmail.com>
2020-10-10 01:33:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,8 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |k, v|
     print "#{k}:
-           #{v}""
-    end 
+           #{v}"
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
